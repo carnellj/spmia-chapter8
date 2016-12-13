58cc530cda2265bd202501c6ae9ad71042c6df6d
carnellj <john.carnell@inin.com>
2016-12-13 17:09:47 -0500

    Removed unecessary code

--- specialroutes-service/src/main/java/com/thoughtmechanix/specialroutes/exception/NoRouteFound.java
+++
@@ -1,9 +0,0 @@
-package com.thoughtmechanix.specialroutes.exception;
-
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.NOT_FOUND)
-public class NoRouteFound extends RuntimeException{
-}
