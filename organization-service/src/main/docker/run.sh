6044f87ff692c4f8124bc8412ec63f5781833482
carnellj <john.carnell@inin.com>
2017-01-09 06:31:45 -0500

    Converted to Docker 2

--- organization-service/src/main/docker/run.sh
+++ organization-service/src/main/docker/run.sh
@@ -1,32 +1,28 @@
 #!/bin/sh
-getPort() {
-    echo $1 | cut -d : -f 3 | xargs basename
-}
-
 echo "********************************************************"
-echo "Waiting for the eureka server to start on port $(getPort $EUREKASERVER_PORT)"
+echo "Waiting for the eureka server to start on port $EUREKASERVER_PORT"
 echo "********************************************************"
-while ! `nc -z eurekaserver  $(getPort $EUREKASERVER_PORT)`; do sleep 3; done
+while ! `nc -z eurekaserver $EUREKASERVER_PORT`; do sleep 3; done
 echo "******* Eureka Server has started"
 
 
 echo "********************************************************"
-echo "Waiting for the database server to start on port $(getPort $DATABASE_PORT)"
+echo "Waiting for the database server to start on port $DATABASESERVER_PORT"
 echo "********************************************************"
-while ! `nc -z database $(getPort $DATABASE_PORT)`; do sleep 3; done
+while ! `nc -z database $DATABASESERVER_PORT`; do sleep 3; done
 echo "******** Database Server has started "
 
 echo "********************************************************"
-echo "Waiting for the configuration server to start on port $(getPort $CONFIGSERVER_PORT)"
+echo "Waiting for the configuration server to start on port $CONFIGSERVER_PORT"
 echo "********************************************************"
-while ! `nc -z configserver $(getPort $CONFIGSERVER_PORT)`; do sleep 3; done
+while ! `nc -z configserver $CONFIGSERVER_PORT`; do sleep 3; done
 echo "*******  Configuration Server has started"
 
 
 echo "********************************************************"
-echo "Waiting for the kafka server to start on port  $(getPort $KAFKASERVER_PORT)"
+echo "Waiting for the kafka server to start on port  $KAFKASERVER_PORT"
 echo "********************************************************"
-while ! `nc -z kafkaserver  $(getPort $KAFKASERVER_PORT)`; do sleep 10; done
+while ! `nc -z kafkaserver $KAFKASERVER_PORT`; do sleep 10; done
 echo "******* Kafka Server has started"
 
 echo "********************************************************"
